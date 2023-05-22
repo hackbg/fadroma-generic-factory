d40652ae26b06b28b83633d211eaf25d4c58a33f
aakamenov <asparuh@hack.bg>
2023-05-22 17:50:52 +0300

    Refactor and add tests.

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,7 @@ use fadroma::{
 use serde::{Serialize, Deserialize, de::DeserializeOwned};
 
 pub const REPLY_ID: u64 = 78024480;
+pub const INSTANCE_ADDR_ATTR: &str = "fadroma_instance_address";
 
 pub trait ExtraData: JsonSchema +
     Serialize + DeserializeOwned +
@@ -27,8 +28,8 @@ pub trait ExtraData: JsonSchema +
 
 #[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
 pub struct InstantiateMsg {
-    admin: Option<String>,
-    code: ContractCode
+    pub admin: Option<String>,
+    pub code: ContractCode
 }
 
 #[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
@@ -46,8 +47,8 @@ pub enum QueryMsg {
 
 #[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
 pub struct InstanceConfig<MSG> {
-    msg: MSG,
-    funds: Vec<Coin>
+    pub msg: MSG,
+    pub funds: Vec<Coin>
 }
 
 #[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
@@ -64,9 +65,9 @@ pub struct Instance<
     A,
     EXTRA: ExtraData
 > {
-    contract: ContractLink<A>,
+    pub contract: ContractLink<A>,
     #[serde(bound = "")] // See https://github.com/serde-rs/serde/issues/1296
-    extra: EXTRA
+    pub extra: EXTRA
 }
 
 #[derive(Serialize, Deserialize, JsonSchema, Clone, Copy, Debug)]
@@ -82,7 +83,7 @@ pub struct PaginatedResponse<T: Serialize> {
 }
 
 pub struct GenericFactory<
-    MSG: Serialize + DeserializeOwned,
+    MSG: Serialize,
     EXTRA: ExtraData = Empty,
     const AUTH: bool = true
 >{
@@ -99,7 +100,7 @@ const CONTRACT: SingleItem<
 namespace!(InstancesNs, b"instances");
 
 impl<
-    MSG: Serialize + DeserializeOwned,
+    MSG: Serialize,
     EXTRA: ExtraData,
     const AUTH: bool
 > GenericFactory<MSG, EXTRA, AUTH> {
@@ -130,7 +131,6 @@ impl<
     pub fn query(
         deps: Deps,
         _env: Env,
-        _info: MessageInfo,
         msg: QueryMsg
     ) -> StdResult<Binary> {
         match msg {
@@ -161,18 +161,23 @@ impl<
             ));
         }
 
-        if let SubMsgResult::Ok(resp) = reply.result {
-            Self::handle_reply(deps, resp)?;
-        }
+        let response = if let SubMsgResult::Ok(resp) = reply.result {
+            let addr = Self::handle_reply(deps, resp)?;
 
-        Ok(Response::default())
+            Response::default()
+                .add_attribute_plaintext(INSTANCE_ADDR_ATTR, addr)
+        } else {
+            Response::default()
+        };
+
+        Ok(response)
     }
 
     /// Lower level function to use when you have additional logic
     /// in your reply handler. Otherwise, use [`GenericFactory::reply`].
     /// You should match the ID of the reply with [`REPLY_ID`] and then
-    /// call this function.
-    pub fn handle_reply(deps: DepsMut, resp: SubMsgResponse) -> StdResult<()> {
+    /// call this function. Returns the address of the new instance.
+    pub fn handle_reply(deps: DepsMut, resp: SubMsgResponse) -> StdResult<Addr> {
         let Some(data) = resp.data else {
             return Err(StdError::generic_err(format!(
                 "Expecting non-empty data in reply of type {}.",
@@ -185,7 +190,7 @@ impl<
         let contract = CONTRACT.load_or_error(deps.storage)?;
         let mut instances = Self::instances();
 
-        let address = data.address.canonize(deps.api)?;
+        let address = data.address.as_ref().canonize(deps.api)?;
         let key = address.clone(); // it is what it is...
         
         instances.insert(
@@ -200,7 +205,7 @@ impl<
             }
         )?;
 
-        Ok(())
+        Ok(data.address)
     }
 
     pub fn create_instance(
@@ -290,6 +295,31 @@ impl<
 
 impl Pagination {
     pub const MAX_LIMIT: u8 = 30;
+
+    #[inline]
+    pub fn new(start: u64, limit: u8) -> Self {
+        Self { start, limit}
+    }
+}
+
+impl InstantiateReplyData<Empty> {
+    #[inline]
+    pub fn new(address: Addr) -> Self {
+        Self {
+            address,
+            extra: Empty { }
+        }
+    }
+}
+
+impl<EXTRA: ExtraData> InstantiateReplyData<EXTRA> {
+    #[inline]
+    pub fn with_extra(address: Addr, extra: EXTRA) -> Self {
+        Self {
+            address,
+            extra
+        }
+    }
 }
 
 impl<T: JsonSchema +
@@ -297,3 +327,283 @@ impl<T: JsonSchema +
     FadromaSerialize + FadromaDeserialize
 > ExtraData for T { }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use fadroma::{
+        core::ContractLink,
+        ensemble::{
+            ContractEnsemble, ContractHarness, AnyResult, MockEnv,
+            ResponseVariants, ExecuteResponse
+        }
+    };
+
+    const ADMIN: &str = "admin";
+
+    impl<
+        MSG: Serialize + DeserializeOwned,
+        EXTRA: ExtraData,
+        const AUTH: bool
+    > ContractHarness for GenericFactory<MSG, EXTRA, AUTH> {
+        fn instantiate(
+            &self,
+            deps: DepsMut,
+            env: Env,
+            info: MessageInfo,
+            msg: Binary
+        ) -> AnyResult<Response> {
+            let result = Self::instantiate(deps, env, info, from_binary(&msg)?)?;
+
+            Ok(result)
+        }
+
+        fn execute(
+            &self,
+            deps: DepsMut,
+            env: Env,
+            info: MessageInfo,
+            msg: Binary
+        ) -> AnyResult<Response> {
+            let result = Self::execute(deps, env, info, from_binary(&msg)?)?;
+
+            Ok(result)
+        }
+
+        fn query(&self, deps: Deps, env: Env, msg: Binary) -> AnyResult<Binary> {
+            let result = Self::query(deps, env, from_binary(&msg)?)?;
+
+            Ok(result)
+        }
+
+        fn reply(&self, deps: DepsMut, env: Env, reply: Reply) -> AnyResult<Response> {
+            let result = Self::reply(deps, env, reply)?;
+
+            Ok(result)
+        }
+    }
+
+    struct Child;
+
+    #[derive(Serialize, Deserialize, JsonSchema, Clone, Debug)]
+    pub struct ChildInstantiateMsg {
+        text: String
+    }
+
+    impl ContractHarness for Child {
+        fn instantiate(
+            &self,
+            _deps: DepsMut,
+            env: Env,
+            _info: MessageInfo,
+            msg: Binary
+        ) -> AnyResult<Response> {
+            let msg: ChildInstantiateMsg = from_binary(&msg)?;
+
+            Ok(Response::new()
+                .set_data(to_binary(&InstantiateReplyData {
+                    address: env.contract.address,
+                    extra: msg.text
+                })?)
+            )
+        }
+
+        fn execute(
+            &self,
+            _deps: DepsMut,
+            _env: Env,
+            _info: MessageInfo,
+            _msg: Binary
+        ) -> AnyResult<Response> {
+            todo!()
+        }
+
+        fn query(&self, _deps: Deps, _env: Env, _msg: Binary) -> AnyResult<Binary> {
+            todo!()
+        }
+    }
+
+    struct Suite {
+        ensemble: ContractEnsemble,
+        factory: ContractLink<Addr>
+    }
+
+    impl Suite {
+        fn new<const AUTH: bool>() -> Self {
+            let mut ensemble = ContractEnsemble::new();
+            let child = ensemble.register(Box::new(Child));
+            let factory = ensemble.register(
+                Box::new(GenericFactory::<ChildInstantiateMsg, String, AUTH> {
+                    msg_phantom: PhantomData,
+                    extra_phantom: PhantomData
+                })
+            );
+
+            let factory = ensemble.instantiate(
+                factory.id,
+                &InstantiateMsg {
+                    admin: None,
+                    code: child
+                },
+                MockEnv::new(ADMIN, "factory")
+            )
+            .unwrap()
+            .instance;
+
+            Self { ensemble, factory }
+        }
+    }
+
+    #[test]
+    fn only_admin_can_instantiate_when_auth_param_is_true() {
+        let Suite { mut ensemble, factory } = Suite::new::<true>();
+
+        let config = InstanceConfig {
+            msg: ChildInstantiateMsg {
+                text: String::from("flaming swords")
+            },
+            funds: Vec::new()
+        };
+        
+        let err = ensemble.execute(
+            &ExecuteMsg::CreateInstance(config.clone()),
+            MockEnv::new("not admin", &factory.address)
+        ).unwrap_err();
+
+        assert_eq!(
+            err.unwrap_contract_error().to_string(),
+            "Generic error: Unauthorized"
+        );
+
+        ensemble.execute(
+            &ExecuteMsg::CreateInstance(config),
+            MockEnv::new(ADMIN, &factory.address)
+        ).unwrap();
+    }
+
+    #[test]
+    fn only_admin_can_instantiate_when_auth_param_is_false() {
+        let Suite { mut ensemble, factory } = Suite::new::<false>();
+
+        let config = InstanceConfig {
+            msg: ChildInstantiateMsg {
+                text: String::from("flaming swords")
+            },
+            funds: Vec::new()
+        };
+
+        ensemble.execute(
+            &ExecuteMsg::CreateInstance(config.clone()),
+            MockEnv::new("not admin", &factory.address)
+        ).unwrap();
+
+        ensemble.execute(
+            &ExecuteMsg::CreateInstance(config),
+            MockEnv::new(ADMIN, &factory.address)
+        ).unwrap();
+    }
+
+    #[test]
+    fn instances_are_stored_with_extra_data() {
+        let Suite { mut ensemble, factory } = Suite::new::<false>();
+
+        let config = InstanceConfig {
+            msg: ChildInstantiateMsg {
+                text: String::from("flaming swords")
+            },
+            funds: Vec::new()
+        };
+
+        let resp = ensemble.execute(
+            &ExecuteMsg::CreateInstance(config.clone()),
+            MockEnv::new("not admin", &factory.address)
+        ).unwrap();
+
+        let addr = extract_instance_addr(&resp);
+
+        let instance: Instance<Addr, String> = ensemble.query(
+            &factory.address,
+            &QueryMsg::InstanceByAddr { addr }
+        )
+        .unwrap();
+
+        assert!(instance.contract.address.as_str().starts_with("fadroma factory child instance"));
+        assert_eq!(instance.contract.code_hash, "test_contract_0");
+        assert_eq!(instance.extra, "flaming swords");
+
+        let instance: Option<Instance<Addr, String>> = ensemble.query(
+            &factory.address,
+            &QueryMsg::InstanceByAddr { addr: "wrong addr".into() }
+        )
+        .unwrap();
+
+        assert!(instance.is_none());
+    }
+
+    #[test]
+    fn list_instances() {
+        let Suite { mut ensemble, factory } = Suite::new::<true>();
+
+        let num_instances: u8 = 10;
+
+        for i in 0..num_instances {
+            let config = InstanceConfig {
+                msg: ChildInstantiateMsg {
+                    text: format!("extra data {i}")
+                },
+                funds: Vec::new()
+            };
+
+            ensemble.execute(
+                &ExecuteMsg::CreateInstance(config),
+                MockEnv::new(ADMIN, &factory.address)
+            ).unwrap();
+        }
+
+        let instances: PaginatedResponse<Instance<Addr, String>> = ensemble.query(
+            &factory.address,
+            &QueryMsg::ListInstances {
+                pagination: Pagination::new(0, num_instances / 2)
+            }
+        ).unwrap();
+
+        assert_eq!(instances.total, num_instances as u64);
+        assert_eq!(instances.entries.len(), (num_instances / 2) as usize);
+
+        for (i, instance) in instances.entries.iter().enumerate() {
+            assert!(instance.contract.address.as_str().starts_with("fadroma factory child instance"));
+            assert_eq!(instance.contract.code_hash, "test_contract_0");
+            assert_eq!(instance.extra, format!("extra data {i}"));
+        }
+
+        let instances: PaginatedResponse<Instance<Addr, String>> = ensemble.query(
+            &factory.address,
+            &QueryMsg::ListInstances {
+                pagination: Pagination::new((num_instances / 2) as u64, num_instances)
+            }
+        ).unwrap();
+
+        assert_eq!(instances.total, num_instances as u64);
+        assert_eq!(instances.entries.len(), (num_instances / 2) as usize);
+
+        for (i, instance) in instances.entries.iter().enumerate() {
+            assert!(instance.contract.address.as_str().starts_with("fadroma factory child instance"));
+            assert_eq!(instance.contract.code_hash, "test_contract_0");
+            assert_eq!(instance.extra, format!("extra data {}", i as u8 + (num_instances / 2)));
+        }
+    }
+
+    fn extract_instance_addr(resp: &ExecuteResponse) -> String {
+        let resp = resp.iter().find(|x| x.is_reply()).expect("no reply response");
+
+        if let ResponseVariants::Reply(reply) = resp {
+            let addr = reply.response.attributes.iter()
+                .find(|x| x.key == INSTANCE_ADDR_ATTR);
+
+            if let Some(addr) = addr {
+                return addr.value.clone();
+            }
+        };
+        
+        panic!("Couldn't find the {}", INSTANCE_ADDR_ATTR);
+    }
+}
